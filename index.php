d16de6a07379233566f6818955dcee8e801ea257
ramojo <amramogo@gmail.com>
2020-07-16 22:05:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="ICIPE Capacity Building and Institutional Development" content="ICIPE Library, Arppis, Drip, Annual Report, ICIPE Scholars, CBID">
+    <meta name="Allan Ramogo, Christian Mumo" content="Designer">
+    <link rel="icon" href="images/icipe.png">
+
+    <title>ICIPE CB&amp;ID: PhD Annual Report 2020</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+   
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <link href="css/theme.css" rel="stylesheet">
+
+<style>
+
+#block-menu-menu-quick-links {
+
+	position: relative;
+    margin: 0.5em;
+    overflow: hidden;
+	padding: 9px 5px 7px 10px;
+	
+}
+
+#block-menu-menu-quick-links ul {
+	text-align: right;
+
+}
+
+#block-menu-menu-quick-links ul li {
+	display: inline;
+
+	flex-grow: 0.25;
+    flex-basis: 0.25;
+    margin: .1em 0;
+    padding: 0 0em;
+    text-align: center;
+    border-left: .1px solid #ccc;
+    background-color: #fff;
+	
+}
+
+#block-menu-menu-quick-links ul li a {
+	display: inherit;
+
+	color: #555555;
+	font-weight: 600;
+	font-family: "Open Sans", sans-serif;
+	
+}
+
+div.banner {
+	background-color: #0C592D;
+	background-image: url("images/student_banner2.jpg"); 
+	
+	height: 250px; 
+	width: 100%;
+	
+	padding: 1em 5em;
+	
+	font-size: 450%;
+	font-family: "Open Sans", sans-serif;
+	font-weight: 200;
+	
+	color: white;
+	
+}
+
+
+div.row.row_bread {
+
+	margin-left: -15px;
+	margin-right: -15px;
+	box-sizing: border-box;	
+	width: 100%;
+}
+
+
+.container_bread {
+	
+	font-family: "Open Sans", sans-serif;
+	font-weight: 600;
+	
+	padding: 0 10em;
+		
+	color: #000000;
+	background-color: transparent;
+	
+	width: 100%;
+	
+	outline: none;
+	outline-offset: -2px;
+	
+}
+ 
+.bread {
+	background-color: rgba(255, 255, 255, 0.25 );
+	font-size: 0.25em;
+	font-weight: 600;
+	
+	color: black;
+	
+	padding: 0.5em;
+	
+	position: absolute;
+	bottom: 26em;
+	left: 0;
+	
+	width: 100%;
+} 
+
+.bread a {
+
+	text-decoration: none;
+	color: inherit;
+	
+}
+ 
+.bread_wrapper{
+
+}
+
+.col-md-4{
+	
+	color:;
+}
+
+
+h3{
+
+	color: white;
+	font-family: "Open Sans", sans-serif;
+	font-weight: 600;
+	background-color: #0A4881; 
+
+}
+
+div.copyright {
+	background-color: #0A4881;
+
+	height: 60px;
+
+	width:100%;
+	
+	font-size: 13px;
+	color: white;
+	text-align: center;
+	padding: 1.5em 4em;
+}
+</style>	
+	
+  </head>
+    
+ <!--*****body*******--> 
+ <body role="document">
+
+<div class="top-logo">
+ <div class="region region-top-logo">
+
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<logo><img src="images/logo.png" height="111px" width="500px"></logo>
+ 
+  </div>
+</div>
+ 
+
+
+        
+                       
+	<!--**********Banner*********-->
+	<div class="banner">
+		<p>PhD Annual Report 2020</p>
+	</div>
+	
+	<!--***********End of Banner**************-->
+	
+	  
+	<!--*******************BODY*********************-->
+
+	<!--<div class="col-sm-offset-1">-->
+<div class="container" style="text-align:justify">
+				<div class=page-header>
+<!--					<h1>PhD Annual Report</h1>-->
+			<p>All icipe PhD scholars who have not yet successfully defended their thesis must prepare an annual report. The purpose of the report is to assess progress through the programme in relation to clearly articulated expectations and milestones, and give scholars feedback on their progress.</p>
+				
+			<p>You can download an Annual Report Form here: <a href="http://cbid.icipe.org/apps/phd_report/resources/PhD_icipe_Annual_Report_Form_2020.docx
+"><b>Annual Report Form</b></a><p>
+
+			<p>The form consists of three parts. Part 1 is to be completed by the PhD Scholar; Part 2 by the Primary icipe Supervisor; and Part 3 by the Primary University Supervisor.</p>
+
+			<p>The Capacity Building and Institutional Development Programme and the icipe Board of Training and Postgraduate Studies (IBTPS) will then review the report and provide feedback to assist the scholar continue to advance through the PhD programme. The IBTPS may also request a meeting with the scholar and their supervisors to discuss progress.</p>
+
+			<p>Please note, there may be good reasons for moving at a slower pace than expected. For example, a scholar may switch supervisors, or laboratory facilities may not be available as expected. A scholar’s progress might also be delayed by major life events, e.g. the birth of a child. These reasons should be clearly explained in the scholar’s self-assessment. These reasons will be taken into account to help the IBTPS determine a rating of progress and make recommendations.</p>
+			
+<!--			<p>There may be good reasons for moving at a slower pace than expected. For example, a scholar may switch supervisors, or laboratory facilities may not be available as expected. A scholar’s progress might also be delayed by major life events (e.g. the birth of a child). These reasons should be clearly explained in the scholar’s self-assessment. These reasons will be taken into account to help the IBTPS determine a rating of progress and formulate recommendations.</p>
+
+			<p>All scholars in a PhD programme at icipe, who have not yet successfully defended a thesis, will participate in the annual report every year.</p>
+-->				
+
+<div class=page-header style="center">
+	<h1>Procedure for scholars</h1>
+</div>
+
+	<ol>
+				<li><p>Download the <a href="http://cbid.icipe.org/apps/phd_report/resources/PhD_icipe_Annual_Report_Form_2018.docx"><b> Annual Report Form</b></a>.</p></li>	
+				<li><p>Complete PART 1 of the form, and sign and date.</p></li>
+				<li><p>After completing PART 1, send the <u>entire</u> form as a Word document to your <em>icipe</em> Primary Supervisor and your University Primary Supervisor by <b>July 20th 2020</b>.</p></li>
+				<li><p>Discuss the report with your supervisors.</p></li>
+				<li><p>.Ask your supervisors to complete, sign and date their respective part (PART 2: <em>icipe</em> Supervisor; PART 3: University Supervisor), and return to you by <u>August 7th 2020</u>. </p></li>
+				<li><p>Compile all parts of the report (PARTs 1, 2 and 3) into a single Word document.</p></li>
+				<li><p>Upload the completed report <a href="http://cbid.icipe.org/apps/cbid/forms/phd_annual_report_2020.php">HERE</a> by <u>August 14th 2020</u>.</p></li>
+	</ol>
+
+				<div class=page-header style="center">
+					<h4>Instructions For Uploading The Report</h4>
+				</div>
+	<ol>
+				<li><p><b></b>Go <a target="_blank" href="http://cbid.icipe.org/apps/cbid/forms/phd_annual_report_2018.php"> <span><b>HERE</b></span></a> to upload the completed 2018 Annual Report.</p>
+				<p>Accepted file formats are: doc, docx or pdf. The file size limit is 5 MB.</p></li>
+				<li><p>Once you have entered your information and uploaded the 2018 Annual Report, click the <b>Continue</b> button.</p></li>  
+				<li><p>	This will take you to the <b>Review your Entries</b> page where you can change anything.</p></li>
+				<li><p>When you are satisfied with your information and upload, click the <b>Continue</b> button to submit your 2020 Annual Report.</p></li> 
+       				 <p>This cannot be undone, so please make sure your information is correct before submitting.</p></li>
+<!-- 				<li><b></b>	We will send you an automatic email acknowledgement when we receive your 2016 Annual Report online.</li>-->
+      </ol>
+      
+ 
+			<p><b>Frequently asked questions</b></p>
+
+
+		<p style="padding-left:2em"><b>Q:</b> I cannot upload my Annual Report. What is wrong?</p>
+
+					<p style="padding-left:2em"><b>A:</b>(1) Your file may be too large. The maximum file size is 5 MB.</p>
+					<p style="padding-left:2em"><b>A:</b>(2) Perhaps you are trying to upload a file that is not one of the accepted file formats (accepted formats: doc, docx)</p>
+
+					
+
+<div class=page-header style="center">
+	<h1>Key dates</h1>
+</div>
+<div class="row" style="center">
+        <div class="col-sm-9">
+          <ul class="list-group">
+			<li class="list-group-item">18th July 2020: Announcement of annual report process.</li>
+
+			<li class="list-group-item">30th July 2020: Deadline for completion of report form by scholar (self-assessment).</li>
+
+			<li class="list-group-item">7th August 2020: Deadline for completion of report by the supervisors.</li>
+
+			<li class="list-group-item">14th August 2020: Deadline for uploading the report onto online platform.</li>
+
+			<li class="list-group-item">1st October 2020: Report forms submitted to IBTPS for review.</li>
+
+			<!--<li class="list-group-item">16th December: All completed report forms submitted to IBTPS for report</li>-->
+
+			<li class="list-group-item">November 2020: IBTPS meet to discuss the annual reports and make recommendations.</li>
+		 </ul>
+        </div><!-- /.col-sm-4 -->
+
+</div>
+
+<div class="page-header">
+<h1>What are the potential outcomes of the Annual Report?</h1>
+</div>
+<p>The IBTPS will review the Annual Report and assign a rating of progress. Those ratings and associated requirements are shown below:<p>
+
+
+          <table class="table table-striped table-bordered table-responsive col-sm-6">
+            <thead>
+              <tr>
+                <th>Progress Rating</th>
+                <th>Description</th>
+                <th>Scholar’s Action Required</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Satisfactory</td>
+                <td>Scholar is making expected level of progress through the programme.</td>
+                <td><ul><li> Scholar continues appropriate work in the programme until the next annual report cycle.</li></ul></td>
+              </tr>
+              <tr>
+                <td>Concern</td>
+                <td>Scholar is not progressing through the programme as expected. Specific reasons for this rating will be identified in the IBTPS Report.</td>
+                <td><ul><li> Scholar meets with her/his primary supervisors to discuss concerns mentioned in the IBTPS report and to make plans to address those concerns.</li>
+                 <li> Within 4 weeks of receipt of the IBTPS Report, the scholar must submit to CB&ID a letter explaining what she/he is doing to improve her/his performance in the programme, together with a revised work plan.</li></ul></td>
+              </tr>
+              <tr>
+                <td>Probation</td>
+                <td>Scholar is failing to meet in a significant manner one or more expectations associated with the programme. Specific reasons for this rating will be identified in the IBTPS report.</td>
+                <td><ul><li>Scholar meets with her/his primary supervisors to discuss concerns mentioned in the IBTPS report and to make plans to address those concerns.</li>
+                <li>Within 4 weeks of receipt of the IBTPS Report, the scholar must submit to CB&ID a letter explaining what she/he is doing to improve her/his performance in the programme, together with a revised work plan.</li>
+                <li>Mid-term report: The scholar will submit a mid-term report by March 31st of the following year. If the IBTPS rates a scholar’s progress as Probation in two report cycles, she/he may be asked to discontinue enrollment in the programme.</li></ul></td>
+              </tr>
+            </tbody>
+          </table>
+<p>&nbsp;</p>
+
+
+ <div class=page-header style="center">
+        <h1>Other questions</h1>
+</div>
+
+			<p>In case of any questions regarding the report, kindly contact:</p>
+                                <p><b>Ms. Vivian Atieno</b></p> 
+                                <p>Capacity Building Officer, </p>
+                                <p>Capacity Building and Institutional Development Programme, <em>icipe</em></p> 
+                                <p><a href="mailto:vatieno@icipe.org"><span>vatieno@icipe.org</span></a></p> 
+                                <!--<p>Tel: +254 20 863 2272</p>-->
+                                <p>&nbsp;</p>
+      
+<!--*******************END of BODY**************-->
+    
+			
+		
+	<!--************footer**************-->
+    <footer>
+      <div class="container">
+        <p> All rights reserved &copy;<?php echo date('Y'); ?> <a target="_blank" href="www.icipe.org"><em>icipe</em></a> </p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+	<!--**********end of footer**********-->
+
+  </body>
+</html>
